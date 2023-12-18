259d8c22b0c92a178872430f23aa8ad89f7aa400
MariiaSpiridonova <mariia.spiridonova@gmail.com>
2023-12-18 23:49:19 +0200

    homework 6

--- hw6/hw_6_2.py
+++ hw6/hw_6_2.py
@@ -4,13 +4,14 @@ from tabulate import tabulate
 n = int(input("Введіть ціле число більше 5 >>> "))
 m = int(input("Введіть ціле число більше 5 >>> "))
 
-matrix = [[random.randint(1, 100) for i in range(n)] for j in range(m)]
+matrix = [[random.randint(1, 100) for i in range(m)] for j in range(n)]
 
 # for line in matrix:
 #     print(*line)
 
 print(tabulate(matrix))
 
-matrix[m-1], matrix[m-2] = matrix[m-2], matrix[m-1]
+for j in range(n):
+    matrix[j][m-1], matrix[j][m-2] = matrix[j][m-2], matrix[j][m-1]
 
 print(tabulate(matrix))
